ac86056b2e6312993b99ae7a80dce427a9063807
Irfan Yahya Abdillah <irfanyahyaa@gmail.com>
2024-09-20 23:42:23 +0700

    feat: add update profile

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Resources\ProfileResource;
 use App\Models\Profile;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
 class ProfileController extends BaseController
@@ -21,5 +22,30 @@ class ProfileController extends BaseController
             return $this->sendError('Profile not found.', 404);
         }
         return $this->sendResponse(new ProfileResource($profile), 'Profile retrieved successfully');
+    }
+
+    public function update(Request $request, $id): JsonResponse
+    {
+        $validator = Validator::make($request->all(), [
+            'full_name' => 'required',
+            'address' => 'required',
+            'gender' => 'required',
+            'marital_status' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return $this->sendError('Validation Error.', 422);
+        }
+
+        $profile = Profile::all()->findOrFail($id);
+
+        $profile->update([
+            'full_name' => $request->full_name,
+            'address' => $request->address,
+            'gender' => $request->gender,
+            'marital_status' => $request->marital_status,
+        ]);
+
+        return $this->sendResponse(new ProfileResource($profile), 'Profile updated successfully');
     }
 }
